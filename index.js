8493621c83efc8e6789c4df98633f649cdee0589
usman.brackets <usmanbrackets@gmail.com>
2022-09-06 21:45:13 +0500

    fear:ad

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const name= "usman";
-let age=12
-var gender='male';
+const app = require("express")();
+
+app.get("/", (req, res) => {
+  return res.status(200).send("server is healthy");
+});
+
+app.listen(5000, () => {
+  console.log("server is running on port 5000");
+});
