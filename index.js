9dbc25a141cb757d50bf1bf62491af3390df351d
Usman <106980152+usmanbrackets@users.noreply.github.com>
2022-09-21 14:34:56 +0500

    asdfsag

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = require("express")();
 console.log("process.env\n\n ", process.env);
 
 app.get("/", (req, res) => {
+  console.log("process.env\n\n ", process.env);
   return res.status(200).send("server is healthy with ci and cd");
 });
 
