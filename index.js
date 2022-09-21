8c79b9bcbfbc4af98a4ebc6e7a0009dc995334fa
Usman <106980152+usmanbrackets@users.noreply.github.com>
2022-09-21 14:27:13 +0500

    asdfsdgi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 
-console.log("process.env ", process.env);
+console.log("process.env\n\n ", process.env);
 
 app.get("/", (req, res) => {
   return res.status(200).send("server is healthy with ci and cd");
