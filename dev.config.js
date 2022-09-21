a2459e412f2a23b5f0ff3177d42d36c7aad3dcd1
Usman <106980152+usmanbrackets@users.noreply.github.com>
2022-09-21 14:37:59 +0500

    adgasgi

--- dev.config.js
+++ dev.config.js
@@ -4,6 +4,8 @@ module.exports = {
       name: "dev-api",
       script: "./index.js",
       env: {
+        error_file: "../logs/error.log",
+        out_file: "../logs/out.log",
         MONITAIR_APP_ENV: "mydummypath",
         NODE_PATH: ".",
         PORT: 8000,
